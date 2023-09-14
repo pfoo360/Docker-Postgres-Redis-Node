a9f362c29f6d03eb144885058e72489ead0b63cf
Phillip Foo <pfoo360@gmail.com>
2023-09-14 01:03:39 -0400

    added typing to postgres db fn params

--- node/src/lib/postgres.ts
+++ node/src/lib/postgres.ts
@@ -1,6 +1,5 @@
 import { Pool } from "pg";
 import fs from "fs";
-import { Insert, Select } from "../types/types";
 
 //postgresql:{user}:{password}@{host}:{port}/{db}
 
@@ -10,7 +9,13 @@ const databaseUrl =
 
 const pool = new Pool({ connectionString: databaseUrl });
 
-const postgres = async ({ queryString, valuesArr }) => {
+const postgres = async ({
+  queryString,
+  valuesArr,
+}: {
+  queryString: string;
+  valuesArr: string[];
+}) => {
   const pg = await pool.connect();
   try {
     const res = await pg.query(queryString, valuesArr);
@@ -22,35 +27,4 @@ const postgres = async ({ queryString, valuesArr }) => {
   }
 };
 
-const insert = async ({ username, password, email }: Insert) => {
-  const db = await pool.connect();
-  try {
-    const res = await db.query(
-      "INSERT INTO users(username, password, email) VALUES($1::text, $2::text, $3::text)",
-      [username, password, email]
-    );
-    return res.rowCount;
-  } catch (err) {
-    console.log(err);
-  } finally {
-    db.release();
-  }
-};
-
-const select = async (o: Select) => {
-  const db = await pool.connect();
-  const [[key, value]] = Object.entries(o);
-  const query = `SELECT * FROM users WHERE ${key}=$1`;
-
-  try {
-    const res = await db.query(query, [value]);
-    console.log(res.rows);
-    return res.rows;
-  } catch (err) {
-    console.log(err);
-  } finally {
-    db.release();
-  }
-};
-
-export { insert, select, postgres };
+export { postgres };
