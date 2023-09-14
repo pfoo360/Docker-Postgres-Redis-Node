295a74dfef1581c3c15514f7cc236f48d8ca24f2
Phillip Foo <pfoo360@gmail.com>
2023-09-13 23:50:08 -0400

    POST route can now push data to db

--- node/src/app.ts
+++ node/src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { postgres } from "./lib/postgres";
 import { Insert } from "./types/types";
 
 const PORT = process.env.PORT || 3000;
@@ -11,14 +12,26 @@ app.get("/", (req, res) => {
   res.send("Hello world!");
 });
 
-app.post("/", (req, res) => {
+app.post("/", async (req, res) => {
   const { username, email, password }: Insert = req.body;
   if (!username || !password || !email)
     return res
       .status(400)
       .send({ message: "Missing username, password, and/or email" });
 
-  res.send("Hello world!");
+  const queryString =
+    "INSERT INTO users(username, password, email) VALUES($1::text, $2::text, $3::text)";
+  const valuesArr = [username, password, email];
+
+  try {
+    const pg_res = await postgres({ queryString, valuesArr });
+    console.log(pg_res);
+
+    res.status(200).send({ message: "INSERT success" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ message: "An error occured" });
+  }
 });
 
 app.listen(PORT, () => {
