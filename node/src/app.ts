bf87397fc727f7f4828af6f0433d9a5aca258b7b
Phillip Foo <pfoo360@gmail.com>
2023-09-13 23:06:54 -0400

    error handling if client does not send complete form data

--- node/src/app.ts
+++ node/src/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { Insert } from "./types/types";
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,6 +12,12 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) => {
+  const { username, email, password }: Insert = req.body;
+  if (!username || !password || !email)
+    return res
+      .status(400)
+      .send({ message: "Missing username, password, and/or email" });
+
   res.send("Hello world!");
 });
 
